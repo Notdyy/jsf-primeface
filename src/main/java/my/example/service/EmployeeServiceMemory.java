82c09f9f6e5beddfa56e750d322a0dddbe316725
Nawaphon Makornsirisri <98356812+Notdyy@users.noreply.github.com>
2024-04-04 09:43:09 +0700

    Update to fix error

--- src/main/java/my/example/service/EmployeeServiceMemory.java
+++ src/main/java/my/example/service/EmployeeServiceMemory.java
@@ -1,3 +1,5 @@
+// EmployeeServiceMemory.java
+
 package my.example.service;
 
 import java.util.ArrayList;
@@ -9,37 +11,36 @@ import my.example.model.Employee;
 
 public class EmployeeServiceMemory  {
 
-	public static HashMap<String, Employee> employeeMap = new HashMap<String, Employee>();
+    public static HashMap<String, Employee> employeeMap = new HashMap<String, Employee>();
 
-	// add(Employee employee): เพิ่มข้อมูลพนักงานใหม่เข้าสู่ระบบ
-	public void add(Employee employee) {
-		employeeMap.put(employee.getId(), employee);
-	}
-	// update(Employee employee): อัปเดตข้อมูลพนักงานที่มีอยู่ในระบบ
-	public int update(Employee employee) {
-		if (employeeMap.containsKey(employee.getId())) {
-			employeeMap.put(employee.getId(),employee);
-			return 1;
-		}else {
-			return 0;
-		}
-	}
-	// search(Employee employee): ค้นหาข้อมูลพนักงานตามเงื่อนไขที่กำหนดและส่งกลับเป็นรายการ
-	public List<Employee> search(Employee employee) {
-		List<Employee> employeeList = new ArrayList<Employee>();
-		for (Map.Entry<String, Employee> entry : employeeMap.entrySet()) {
-			employeeList.add(entry.getValue());
-		}
-		return employeeList;
-	}
-	// delete(String id): ลบข้อมูลพนักงานจากระบบโดยใช้ id เป็นตัวกำหนด
-	public int delete(String id) {
-		if (employeeMap.containsKey(id)) {
-			employeeMap.remove(id);
-			return 1;
-		}else {
-			return 0;
-		}
-	}
+    public void add(Employee employee) {
+        employeeMap.put(employee.getId(), employee);
+    }
+
+    public int update(Employee employee) {
+        if (employeeMap.containsKey(employee.getId())) {
+            employeeMap.put(employee.getId(),employee);
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+
+    public List<Employee> search(Employee employee) {
+        List<Employee> employeeList = new ArrayList<Employee>();
+        for (Map.Entry<String, Employee> entry : employeeMap.entrySet()) {
+            employeeList.add(entry.getValue());
+        }
+        return employeeList;
+    }
+
+    public int delete(String id) {
+        if (employeeMap.containsKey(id)) {
+            employeeMap.remove(id);
+            return 1;
+        }else {
+            return 0;
+        }
+    }
 
 }
--- src/main/java/my/example/view/CrudBean.java
+++ src/main/java/my/example/view/CrudBean.java
@@ -1,3 +1,5 @@
+// CrudBean.java
+
 package my.example.view;
 
 import java.io.Serializable;
@@ -7,97 +9,107 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.event.SelectEvent;
+
 import my.example.model.Employee;
 import my.example.service.EmployeeServiceMemory;
 
 @ManagedBean
 @ViewScoped
 public class CrudBean implements Serializable {
-    private static final long serialVersionUID = 1L;
-    
-    private String mode;
-    private Employee employeeCriteria;
-    private Employee employeeEdit;
-    private Employee selectedMember;
-    private List<Employee> employeeList;
-    private final EmployeeServiceMemory service = new EmployeeServiceMemory();
-    
-    @PostConstruct
-    // init(): เมทอดที่ถูกเรียกเมื่อ Bean ถูกสร้างขึ้น ใช้ในการกำหนดค่าเริ่มต้น
-    public void init() {
-        mode = "R";
-        employeeCriteria = new Employee(); 
-    }
-    // searchBtnOnclick(): เมทอดที่ถูกเรียกเมื่อผู้ใช้กดปุ่มค้นหา เพื่อทำการค้นหาข้อมูลพนักงาน
-    public void searchBtnOnclick() {
-        employeeList = service.search(employeeCriteria);
-    }
-    // addBtnOnclick(): เมทอดที่ถูกเรียกเมื่อผู้ใช้กดปุ่มเพิ่ม เพื่อทำการเพิ่มข้อมูลพนักงานใหม่
-    public void addBtnOnclick() {
-        mode = "C";
-        employeeEdit = new Employee();
-    }
-    // editBtnOnclick(Employee p): เมทอดที่ถูกเรียกเมื่อผู้ใช้กดปุ่มแก้ไข เพื่อทำการแก้ไขข้อมูลพนักงานที่เลือก
-    public void editBtnOnclick(Employee p) {
-        mode = "U";
-        employeeEdit = p;
-    }
-    // เมทอด saveBtnOnclick(): เมทอดที่ถูกเรียกเมื่อผู้ใช้กดปุ่มบันทึก เพื่อทำการบันทึกข้อมูลพนักงาน
-    public void saveBtnOnclick() {
-        service.add(employeeEdit);
-        mode = "U";
-    }
-    // updateBtnOnclick(): เมทอดที่ถูกเรียกเมื่อผู้ใช้กดปุ่มอัปเดต เพื่อทำการอัปเดตข้อมูลพนักงาน
-    public void updateBtnOnclick() {
-        service.update(employeeEdit);
-    }
-    // deleteBtnOnclick(): เมทอดที่ถูกเรียกเมื่อผู้ใช้กดปุ่มลบ เพื่อทำการลบข้อมูลพนักงาน
-    public void deleteBtnOnclick() {
-        service.delete(employeeEdit.getId());
-    }
-    // backBtnOnclick(): เมทอดที่ถูกเรียกเมื่อผู้ใช้กดปุ่มย้อนกลับ เพื่อกลับไปยังหน้าค้นหาหรือหน้าหลัก
-    public void backBtnOnclick() {
-        mode = "R";
-    }
+	
+	private static final long serialVersionUID = 1L;
 
-    public String getMode() {
-        return mode;
-    }
+	private String mode;
+	private Employee employeeCriteria;
+	private Employee employeeEdit;
+	private Employee selectedMember;
+	private List<Employee> employeeList;
+	
+	private final EmployeeServiceMemory service = new EmployeeServiceMemory();
 
-    public void setMode(String mode) {
-        this.mode = mode;
-    }
+	@PostConstruct
+	public void init() {
+		mode = "R";
+		employeeCriteria = new Employee();
+	}
 
-    public Employee getEmployeeCriteria() {
-        return employeeCriteria;
-    }
+	public void searchBtnOnclick() {
+		employeeList = service.search(employeeCriteria);
+	}
 
-    public void setEmployeeCriteria(Employee employeeCriteria) {
-        this.employeeCriteria = employeeCriteria;
-    }
+	public void addBtnOnclick() {
+		mode = "C";
+		employeeEdit = new Employee();
+	}
 
-    public Employee getEmployeeEdit() {
-        return employeeEdit;
-    }
+	public void editBtnOnclick(Employee p) {
+		mode = "U";
+		employeeEdit = p;
+	}
 
-    public void setEmployeeEdit(Employee employeeEdit) {
-        this.employeeEdit = employeeEdit;
-    }
+	public void saveBtnOnclick() {
+		service.add(employeeEdit);
+		mode = "U";
+	}
 
-    public Employee getSelectedMember() {
-        return selectedMember;
-    }
+	public void updateBtnOnclick() {
+		service.update(employeeEdit);
+	}
 
-    public void setSelectedMember(Employee selectedMember) {
-        this.selectedMember = selectedMember;
-    }
+	public void deleteBtnOnclick() {
+		service.delete(employeeEdit.getId());
+	}
 
-    public List<Employee> getEmployeeList() {
-        return employeeList;
-    }
+	public void backBtnOnclick() {
+		mode = "R";
+	}
 
-    public void setEmployeeList(List<Employee> employeeList) {
-        this.employeeList = employeeList;
-    }
-    
+	public String getMode() {
+		return mode;
+	}
+
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
+
+	public Employee getEmployeeCriteria() {
+		return employeeCriteria;
+	}
+
+	public void setEmployeeCriteria(Employee employeeCriteria) {
+		this.employeeCriteria = employeeCriteria;
+	}
+
+	public Employee getEmployeeEdit() {
+		return employeeEdit;
+	}
+
+	public void setEmployeeEdit(Employee employeeEdit) {
+		this.employeeEdit = employeeEdit;
+	}
+
+	public Employee getSelectedMember() {
+		return selectedMember;
+	}
+
+	public void setSelectedMember(Employee selectedMember) {
+		this.selectedMember = selectedMember;
+	}
+
+	public List<Employee> getEmployeeList() {
+		return employeeList;
+	}
+
+	public void setEmployeeList(List<Employee> employeeList) {
+		this.employeeList = employeeList;
+	}
+
+	public void onRowSelect(SelectEvent event) {
+		// ดึงข้อมูลจากแถวที่ถูกเลือก
+		selectedMember = (Employee) event.getObject();
+		// เรียกใช้เมทอดที่ต้องการเมื่อแถวถูกเลือก
+		editBtnOnclick(selectedMember);
+	}
+
 }
